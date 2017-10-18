d18290369cc964b23a7411c5f017d55645f69039
Sascha <sjhesse@gmail.com>
2017-10-18 23:20:23 +0200

    patched from NTLM to HTTPBasicAuth in connections

--- pyexchange-0.6/pyexchange/connection.py
+++ pyexchange-0.6/pyexchange/connection.py
@@ -6,6 +6,7 @@ Unless required by applicable law or agreed to in writing, software?distributed 
 """
 import requests
 from requests_ntlm import HttpNtlmAuth
+from requests.auth import HTTPBasicAuth
 
 import logging
 
@@ -39,7 +40,7 @@ class ExchangeNTLMAuthConnection(ExchangeBaseConnection):
 
     log.debug(u'Constructing password manager')
 
-    self.password_manager = HttpNtlmAuth(self.username, self.password)
+    self.password_manager = HTTPBasicAuth(self.username, self.password)
 
     return self.password_manager
 
